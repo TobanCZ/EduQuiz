01b64005c6c285e0a1171170b26d0adc8efbb0ac
TobanCZ <65807074+TobanCZ@users.noreply.github.com>
2023-09-08 10:25:20 +0200

    awd

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from the server!' });
